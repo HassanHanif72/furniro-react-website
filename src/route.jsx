6a4e2be6beb81d37e8a73ab763bf4eeecf8dbb6e
unknown <you@example.com>
2024-10-21 02:38:54 +0500

    first commit

--- src/route.jsx
+++ src/route.jsx
@@ -4,40 +4,40 @@ import Header from "./components/Header";
 import About from "./pages/About";
 import Shop from "./pages/Shop";
 import Contact from "./pages/Contact";
-import Productcomparison from "./pages/ProductComparison";
+import ProductComparison from "./pages/ProductComparison"; // Fixed casing
 import LoginForm from "./pages/Login";
 import SignUp from "./pages/Signup";
 import ProductDetails from "./components/ProductDetail";
 import Cart from "./pages/Cart";
 import Wishlist from "./pages/Wishlist";
 import CheckoutPage from "./pages/Checkout";
-import ScrollToTop from "./components/ScrollToTop";  // Import ScrollToTop component
+import ScrollToTop from "./components/ScrollToTop";
 import AdminPanel from "./pages/AdminPanel";
 import ToShip from "./pages/ToShip";
+import NotFound from "./pages/NotFound"; // Optional: Import a 404 page
 
 function AppRouter() {
   return (
-    <>
-      <BrowserRouter>
-        <ScrollToTop /> {/* Add ScrollToTop here */}
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Login" element={<LoginForm />} />
-          <Route path="/Signup" element={<SignUp />} />
-          <Route path="About" element={<About />} />
-          <Route path="/Shop" element={<Shop />} />
-          <Route path="/Contact" element={<Contact />} />
-          <Route path="/Cart" element={<Cart />} />
-          <Route path="/Wishlist" element={<Wishlist />} />
-          <Route path="/Product/:id" element={<ProductDetails />} />
-          <Route path="/AdminPanel" element={<AdminPanel />} />
-          <Route path="/ToShip" element={<ToShip />} />
-          <Route path="/ProductComparison" element={<Productcomparison />} />
-          <Route path="/Checkout" element={<CheckoutPage />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <ScrollToTop />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginForm />} /> {/* Changed to lowercase */}
+        <Route path="/signup" element={<SignUp />} /> {/* Changed to lowercase */}
+        <Route path="/about" element={<About />} /> {/* Changed to lowercase */}
+        <Route path="/shop" element={<Shop />} /> {/* Changed to lowercase */}
+        <Route path="/contact" element={<Contact />} /> {/* Changed to lowercase */}
+        <Route path="/cart" element={<Cart />} /> {/* Changed to lowercase */}
+        <Route path="/wishlist" element={<Wishlist />} /> {/* Changed to lowercase */}
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/adminpanel" element={<AdminPanel />} /> {/* Changed to lowercase */}
+        <Route path="/toshop" element={<ToShip />} /> {/* Changed to lowercase */}
+        <Route path="/productcomparison" element={<ProductComparison />} /> {/* Fixed casing */}
+        <Route path="/checkout" element={<CheckoutPage />} /> {/* Changed to lowercase */}
+        <Route path="*" element={<NotFound />} /> {/* Catch-all route for 404 */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
